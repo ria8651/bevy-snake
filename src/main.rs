c208bafa84dc2adc4eac4912c58205c9a7d8df18
Ria8651 <bk.dev@icloud.com>
2022-05-11 11:33:24 +1200

    Full interpolation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use bevy::{
-    pbr::Material,
     prelude::*,
     render::{camera::ScalingMode, mesh::PrimitiveTopology},
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
@@ -10,6 +9,8 @@ use std::collections::{HashMap, VecDeque};
 #[derive(Component)]
 struct Snake {
     body: Vec<IVec2>,
+    head_dir: IVec2,
+    tail_dir: IVec2,
 }
 
 struct Board {
@@ -47,7 +48,7 @@ fn main() {
             colour1: Color::rgb(0.3, 0.3, 0.3),
             colour2: Color::rgb(0.2, 0.2, 0.2),
         })
-        .insert_resource(MovmentTimer(Timer::from_seconds(1.0, true)))
+        .insert_resource(MovmentTimer(Timer::from_seconds(1.0 / 8.0, true)))
         .insert_resource(InputQueue(VecDeque::new()))
         .insert_resource(Apples {
             list: HashMap::new(),
@@ -82,8 +83,6 @@ fn scene_setup(
     mut commands: Commands,
     b: Res<Board>,
     mut apples: ResMut<Apples>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
     assets: Res<AssetServer>,
 ) {
     apples.sprite = Some(assets.load("images/apple.png"));
@@ -148,9 +147,11 @@ fn snake_setup(
             IVec2::new(3, 7),
             IVec2::new(2, 7),
         ],
+        head_dir: IVec2::new(1, 0),
+        tail_dir: IVec2::new(-1, 0),
     };
 
-    let mesh = mesh_snake(&snake, IVec2::new(1, 0), 0.0);
+    let mesh = mesh_snake(&snake, 0.0);
 
     commands
         .spawn_bundle(MaterialMesh2dBundle {
@@ -237,18 +238,19 @@ fn snake_system(
             spawn_apple(pos, &mut apples, &mut commands, &b);
         } else {
             let len = snake.body.len();
+            snake.tail_dir = snake.body[len - 2] - snake.body[len - 1];
             snake.body.remove(len - 1);
         }
     }
 
     let interpolation = timer.0.elapsed_secs() / timer.0.duration().as_secs_f32() - 0.5;
-    let forward = if let Some(dir) = input_queue.0.get(0) {
+    snake.head_dir = if let Some(dir) = input_queue.0.get(0) {
         DIR[*dir as usize].into()
     } else {
         head - neck
     };
 
-    let mesh = mesh_snake(&snake, forward, interpolation);
+    let mesh = mesh_snake(&snake, interpolation);
     *mesh_handle = meshes.add(mesh).into();
 }
 
@@ -256,9 +258,7 @@ fn end_game() {
     println!("DEEEAAAAATTHHHHH!!!!");
 }
 
-fn mesh_snake(snake: &Snake, forward: IVec2, interpolation: f32) -> Mesh {
-    let mut snake_mesh = Mesh::new(PrimitiveTopology::TriangleList);
-
+fn mesh_snake(snake: &Snake, interpolation: f32) -> Mesh {
     let mut verticies = Vec::new();
 
     fn push_quad(
@@ -312,15 +312,15 @@ fn mesh_snake(snake: &Snake, forward: IVec2, interpolation: f32) -> Mesh {
     }
 
     let width = 0.6;
+    let head_size = 0.7;
 
     let head = snake.body[0];
     let neck = snake.body[1];
     let len = snake.body.len();
     let tail = snake.body[len - 1];
-    let tail_dir = snake.body[len - 2] - tail;
 
-    let mut last = head;
     let mut start = 1;
+    let mut end = len - 1;
 
     if interpolation >= 0.0 {
         start = 0;
@@ -331,44 +331,66 @@ fn mesh_snake(snake: &Snake, forward: IVec2, interpolation: f32) -> Mesh {
             head,
             Vec2::new(0.0, interpolation / 2.0),
             Vec2::new(width / 2.0, interpolation / 2.0),
-            calculate_flip(forward),
+            calculate_flip(snake.head_dir),
         );
-        push_circle(&mut verticies, last, forward.as_vec2() * interpolation, 0.3);
+        push_circle(
+            &mut verticies,
+            head,
+            snake.head_dir.as_vec2() * interpolation,
+            head_size / 2.0,
+        );
 
-        // // Interpolate tail
-        // push_quad(
-        //     &mut verticies,
-        //     tail,
-        //     Vec2::new(0.0, interpolation / 2.0),
-        //     Vec2::new(width / 2.0, interpolation / 2.0),
-        //     calculate_flip(tail_dir),
-        // );
-        // push_circle(
-        //     &mut verticies,
-        //     tail,
-        //     tail_dir.as_vec2() * interpolation,
-        //     0.3,
-        // );
+        // Interpolate tail
+        let tail_dir = snake.body[len - 2] - snake.body[len - 1];
+        push_quad(
+            &mut verticies,
+            tail,
+            Vec2::new(0.0, interpolation / 2.0 + 0.25),
+            Vec2::new(width / 2.0, -interpolation / 2.0 + 0.25),
+            calculate_flip(tail_dir),
+        );
+        push_circle(
+            &mut verticies,
+            tail,
+            tail_dir.as_vec2() * interpolation,
+            width / 2.0,
+        );
     } else {
+        end = len;
+
         // Interpolate head
         push_quad(
             &mut verticies,
             head,
             Vec2::new(0.0, interpolation / 2.0 - 0.25),
             Vec2::new(width / 2.0, interpolation / 2.0 + 0.25),
-            calculate_flip(last - neck),
+            calculate_flip(head - neck),
         );
         push_circle(
             &mut verticies,
-            last,
-            (last - neck).as_vec2() * interpolation,
-            0.3,
+            head,
+            (head - neck).as_vec2() * interpolation,
+            head_size / 2.0,
         );
 
         // Interpolate tail
+        push_quad(
+            &mut verticies,
+            tail,
+            Vec2::new(0.0, interpolation / 2.0),
+            Vec2::new(width / 2.0, -interpolation / 2.0),
+            calculate_flip(snake.tail_dir),
+        );
+        push_circle(
+            &mut verticies,
+            tail,
+            snake.tail_dir.as_vec2() * interpolation,
+            width / 2.0,
+        );
     }
 
-    for i in start..len {
+    let mut last = head;
+    for i in start..end {
         let pos = snake.body[i];
 
         push_circle(&mut verticies, pos, Vec2::new(0.0, 0.0), width / 2.0);
@@ -408,6 +430,7 @@ fn mesh_snake(snake: &Snake, forward: IVec2, interpolation: f32) -> Mesh {
         uvs.push([0.0, 0.0]);
     }
 
+    let mut snake_mesh = Mesh::new(PrimitiveTopology::TriangleList);
     snake_mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
     snake_mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
     snake_mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
